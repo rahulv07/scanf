8cb4cbaf915b9beaae4fbb9fb19fd4d744d148e9
Arul Virumbi <arul.virumbi@gmail.com>
2021-11-27 12:16:47 +0530

    color change

--- bio_guard/lib/main.dart
+++ bio_guard/lib/main.dart
@@ -13,7 +13,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       theme: ThemeData(
-        primarySwatch: Colors.blue,
+        primarySwatch: Colors.green,
       ),
       home: HomePage(),
     );
