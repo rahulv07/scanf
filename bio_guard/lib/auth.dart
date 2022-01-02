acab8d23351b076e2501ade0b06ceee46dac59db
Arul Virumbi <arul.virumbi@gmail.com>
2022-01-02 15:02:43 +0530

    verify fingerprint

---
+++ bio_guard/lib/auth.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+import 'package:local_auth/local_auth.dart';
+
+class Authentication {
+  static SnackBar customSnackBar({required String content}) {
+    return SnackBar(
+      backgroundColor: Colors.black,
+      content: Text(
+        content,
+        style: const TextStyle(color: Colors.redAccent, letterSpacing: 0.5),
+      ),
+    );
+  }
+  static Future<bool> authenticateWithBiometrics() async {
+    final LocalAuthentication localAuthentication = LocalAuthentication();
+    bool isBiometricSupported = await localAuthentication.isDeviceSupported();
+    bool canCheckBiometrics = await localAuthentication.canCheckBiometrics;
+
+    bool isAuthenticated = false;
+
+    if (isBiometricSupported && canCheckBiometrics) {
+      isAuthenticated = await localAuthentication.authenticate(
+        localizedReason: 'Please complete the biometrics to proceed.',
+        biometricOnly: true,
+      );
+    }
+
+    return isAuthenticated;
+  }
+}
