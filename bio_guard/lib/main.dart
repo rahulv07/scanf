f14acf85cd4db8cf559b6ddc1d223288601da8f8
Arul Virumbi <arul.virumbi@gmail.com>
2022-01-02 15:01:10 +0530

    verify fingerprint

--- bio_guard/lib/main.dart
+++ bio_guard/lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
-import 'screens/homepage.dart';
+
+import 'check.dart';
 
 void main() {
   runApp(const MyApp());
@@ -12,10 +13,65 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      title: 'Flutter Demo',
       theme: ThemeData(
-        primarySwatch: Colors.green,
+
+        primarySwatch: Colors.blue,
       ),
-      home: HomePage(),
+      home: const MyHomePage(title: 'BIO GUARD'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({Key? key, required this.title}) : super(key: key);
+
+
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  int _counter = 0;
+
+  void _incrementCounter() {
+    setState(() {
+
+      _counter++;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+
+    return Scaffold(
+      appBar: AppBar(
+        // Here we take the value from the MyHomePage object that was created by
+        // the App.build method, and use it to set our appbar title.
+        title: Text(widget.title),
+      ),
+      body: Center(
+        // Center is a layout widget. It takes a single child and positions it
+        // in the middle of the parent.
+        child: Column(
+
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            RaisedButton(
+                child: Text('Enter'),
+                onPressed: () =>  Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                    builder: (context) => Register(),
+                  ),
+                ),
+            ),
+          ],
+        ),
+      ),
+     // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
